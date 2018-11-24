d3a77ac1658eca32cd74f9f7d3cdb9a366ed0168
pumpingmonster04b63 <pumpingmonster04b63@gmail.com>
2018-11-24 13:45:20 +0200

    project: add eslinter

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
+  baseUrl: process.env.NODE_ENV === 'production'
     ? '//akinto.pro'
     : '/',
-    indexPath: 'index.tpl.html'
+  indexPath: 'index.tpl.html',
+  lintOnSave: true,
+
+  devServer: {
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  }
 }
